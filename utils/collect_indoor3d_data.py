37889a1e8bbfe0b259350a81f54416cd9e259c9c
Buding-97 <xuebing@sues.edu.cn>
2021-11-16 16:33:47 +0800

    update visualization

--- utils/collect_indoor3d_data.py
+++ utils/collect_indoor3d_data.py
@@ -11,12 +11,12 @@ if not os.path.exists(output_folder):
 # Note: there is an extra character in the v1.2 data in Area_5/hallway_6. It's fixed manually.
 for anno_path in anno_paths:
     print(anno_path)
-    # try:
-    elements = anno_path.split('/')
-    out_filename = '{}_{}.npy'.format(elements[-3],elements[-2])  # Area_1_hallway_1.npy
-    if os.path.exists(os.path.join(output_folder, out_filename)):
-        continue
-    indoor3d_util.collect_point_label(anno_path, os.path.join(output_folder, out_filename), 'numpy')
-    # except:
-    #     print(anno_path, 'ERROR!')
-    exit()
+    try:
+        elements = anno_path.split('/')
+        out_filename = '{}_{}.npy'.format(elements[-3],elements[-2])  # Area_1_hallway_1.npy
+        if os.path.exists(os.path.join(output_folder, out_filename)):
+            continue
+        indoor3d_util.collect_point_label(anno_path, os.path.join(output_folder, out_filename), 'numpy')
+    except:
+        print(anno_path, 'ERROR!')
+print('collect over!')
--- utils/visualization.py
+++ utils/visualization.py
@@ -6,25 +6,33 @@ def raw_plyfile():
     pointcloud = np.load('data/stanford_indoor3d_ins.sem/Area_1_conferenceRoom_1.npy')
     color = (pointcloud[:, 3:6] * 255).astype(np.uint8)
     field_names = ['x', 'y', 'z', 'red', 'green', 'blue']
-    write_ply('w.ply', [pointcloud[:, 0:3], color], field_names)
+    write_ply('raw.ply', [pointcloud[:, 0:3], color], field_names)
 
 
 def sem_plyfile():
-    pointcloud = np.load('data/stanford_indoor3d_ins.sem/Area_1_conferenceRoom_1.npy')
-    color = (pointcloud[:, 3:6] * 255).astype(np.uint8)
+    pointcloud = np.load('../data/stanford_indoor3d_ins.sem/Area_1_conferenceRoom_1.npy')
+    semvalue = np.unique(pointcloud[:,6])
+    color = np.random.randint(255, size=(len(semvalue),3), dtype=np.uint8)
+    colors = np.empty((pointcloud.shape[0],3),dtype=np.uint8)
+    for num,i in enumerate(semvalue):
+        colors[pointcloud[:,6] == i] = color[num]
     field_names = ['x', 'y', 'z', 'red', 'green', 'blue']
-    write_ply('w.ply', [pointcloud[:, 0:3], color], field_names)
+    write_ply('sem.ply', [pointcloud[:, 0:3], colors], field_names)
 
 
 def ins_plyfile():
-    pointcloud = np.load('data/stanford_indoor3d_ins.sem/Area_1_conferenceRoom_1.npy')
-    color = (pointcloud[:, 3:6] * 255).astype(np.uint8)
+    pointcloud = np.load('../data/stanford_indoor3d_ins.sem/Area_1_conferenceRoom_1.npy')
+    insvalue = np.unique(pointcloud[:,7])
+    color = np.random.randint(255, size=(len(insvalue),3), dtype=np.uint8)
+    colors = np.empty((pointcloud.shape[0],3),dtype=np.uint8)
+    for num,i in enumerate(insvalue):
+        colors[pointcloud[:,7] == i] = color[num]
     field_names = ['x', 'y', 'z', 'red', 'green', 'blue']
-    write_ply('w.ply', [pointcloud[:, 0:3], color], field_names)
+    write_ply('ins.ply', [pointcloud[:, 0:3], colors], field_names)
 
 
 def main():
-    raw_plyfile()
+    ins_plyfile()
 
 
 if __name__ == '__main__':
